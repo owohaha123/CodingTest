409527dfa3b74ed89aa15c572a9a10879971556a
김윤주 <owohaha123@gmail.com>
2023-05-03 20:12:32 +0900

    mvc 예제_등록

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,17 +1,54 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
+    // 1. 생성자 주입 (기본)
     private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
-
     }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+    // 2. setter 주입 (public 으로 노출. 변경의 여지가 있기에 비선호)
+    //        private MemberService memberService;
+    //
+    //        @Autowired
+    //        public void setMemberService(MemberService memberService) {
+    //            this.memberService = memberService;
+    //        }
+
+    // 3. 필드 주입 (비선호)
+    //    @Autowired private MemberService memberService;
 }
